756810e005d521ef71a5b3fd69ea6ea081a8f109
Minigrim0 <grimauflorent@gmail.com>
2021-08-13 11:28:15 +0200

    [Add] component loading (Display doesn't work)

--- src/main.c
+++ src/main.c
@@ -1,12 +1,61 @@
+#include <stdlib.h>
 #include <gint/display.h>
 #include <gint/keyboard.h>
 
-int main(void)
-{
-	dclear(C_WHITE);
-	dtext(1, 1, C_BLACK, "Sample fxSDK add-in.");
-	dupdate();
+typedef struct {
+    const bopti_image_t *image;
+    const bopti_image_t *image_rot;
+    const char* name;
+} Component;
 
-	getkey();
-	return 1;
+void loadComponents(Component **components){
+    extern bopti_image_t vertical_self;
+    extern bopti_image_t horizontal_self;
+    extern bopti_image_t vertical_capa;
+    extern bopti_image_t horizontal_capa;
+    extern bopti_image_t vertical_resistor;
+    extern bopti_image_t horizontal_resistor;
+
+    *components = (Component*) malloc(3 * sizeof(Component));
+
+    Component self = {&vertical_self, &horizontal_self, "self"};
+    *components[0] = self;
+    Component capa = {&vertical_capa, &horizontal_capa, "capa"};
+    *components[1] = capa;
+    Component resistor = {&vertical_resistor, &horizontal_resistor, "resistor"};
+    *components[2] = resistor;
+}
+
+int drawUI(int cursor_position, Component* components){
+    short int x = 0;
+
+    for(x=0;x<3;x=x+1){
+        drect_border(106, x*21, 127, (x*21)+21, C_WHITE, 1, C_BLACK);
+        dprint(107, x+1, C_BLACK, components[x].name);
+        // dimage(107, x+1, components[x].image);
+    }
+    return 0;
+}
+
+int main(void){
+    extern font_t font;
+    Component* components = NULL;
+
+    dfont(&font);
+
+    loadComponents(&components);
+    dclear(C_WHITE);
+    dprint(107, 0, C_BLACK, components[0].name);
+    dprint(107, 15, C_BLACK, components[1].name);
+    dprint(107, 30, C_BLACK, components[2].name);
+    dupdate();
+    getkey();
+
+    // drawUI(0, components);
+
+    // dupdate();
+    // getkey();
+
+    free(components);
+    return 1;
 }
